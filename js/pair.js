861b74f8e676d2d41f787d90b2e473e4213e6682
juhee <wngmlchoi@naver.com>
2022-10-16 16:13:35 +0900

    20221016

---
+++ js/pair.js
@@ -0,0 +1,15 @@
+// side_menu Click
+const nonClick = document.querySelector(".gray");
+
+function handleClick(event) {
+  // div에서 모든 "click" 클래스 제거
+  nonClick.forEach((e) => {
+    e.classList.remove("click");
+  });
+  // 클릭한 div만 "click"클래스 추가
+  event.target.classList.add("click");
+}
+
+nonClick.forEach((e) => {
+  e.addEventListener("click", handleClick);
+});
