b6bc8847743d0ab7ec79e49e0abd8d64d59b2a7b
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-10-17 16:09:51 +0900

    20221017

--- js/pair.js
+++ js/pair.js
@@ -1,15 +1,26 @@
-// side_menu Click
-const nonClick = document.querySelector(".gray");
+// // side_menu Click
+// const nonClick = document.querySelector(".gray");
 
-function handleClick(event) {
-  // div에서 모든 "click" 클래스 제거
-  nonClick.forEach((e) => {
-    e.classList.remove("click");
-  });
-  // 클릭한 div만 "click"클래스 추가
-  event.target.classList.add("click");
-}
+// function handleClick(event) {
+//     // div에서 모든 "click" 클래스 제거
+//     nonClick.forEach((e) => {
+//         e.classList.remove("click");
+//     });
+//     // 클릭한 div만 "click"클래스 추가
+//     event.target.classList.add("click");
+// }
 
-nonClick.forEach((e) => {
-  e.addEventListener("click", handleClick);
-});
+// nonClick.forEach((e) => {
+//     e.addEventListener("click", handleClick);
+// });
+
+// slick slider
+$(function () {
+    $(".slick_slider").slick({
+        arrows: false,
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+
+    })
+})
