f7909adef6bdc517f58640adca9aad34ffe88fa1
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-10-19 17:45:25 +0900

    20221019

--- js/pair.js
+++ js/pair.js
@@ -6,8 +6,6 @@ $(function () {
     vertical: true,
     autoplaySpeed: 1500,
   });
-});
-$(function () {
   $(".alert_slick").slick({
     asNavFor: '.slick_slider',
     arrows: false,
@@ -15,4 +13,25 @@ $(function () {
     vertical: true,
     autoplaySpeed: 1500,
   });
+  $('#colum_left').on('scroll', function () {
+    var sct = $('#colum_left').scrollTop();
+    console.log(sct, "scrroll")
+
+    if (sct > 1500) {
+      $('.content_code').fadeOut();
+    } else {
+      $('.content_code').fadeIn()
+    }
+  });
+  console.log(0)
 });
+
+
+// //스크롤 이벤트와 화살표 함수
+// $(window).on("scroll", () => {
+//   let wct = $(window).scrollTop();
+//   //top 화살표 나오고 사라지기
+//   const tTop = $(".code_match");
+//   wct > 500 ? tTop.fadeIn() : tTop.fadeOut();
+
+// });
